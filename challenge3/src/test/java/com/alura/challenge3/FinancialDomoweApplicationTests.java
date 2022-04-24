ce20743e86eb9af27b05a2246e318bb2620e366e
juliana.blicharz <juliana.blicharz@hypeflame.com.br>
2022-04-24 16:04:27 -0300

    initial project - 1st week

---
+++ challenge3/src/test/java/com/alura/challenge3/FinancialDomoweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challenge3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialDomoweApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
